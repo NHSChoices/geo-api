190249066d19c7ee0c4b4ed931cb8afd6496c458
Russell <russell@russelldunphy.com>
2013-08-30 16:58:42 +0100

    :mag: Searchbox config for elasticsearch

--- bin/load.sh
+++ bin/load.sh
@@ -1,6 +1,9 @@
 set -e
 
-curl -XDELETE localhost:9200/geo
+url=$1
+
+curl -XDELETE $url/geo
+curl -XPOST $url/geo
 
 tail -n +2 data/places.csv |
   awk -F"," '{\
@@ -13,35 +16,35 @@ long = $8
 print "{ \"index\" :  { \"_index\" : \"geo\", \"_type\" : \"place\", \"_id\" : \"" key "\" } }\
 { \"name\" : [\"" name "\"], \"latitude\" : "lat", \"longitude\" : " long " }" }' > data/places.json
 
-curl -s -XPOST localhost:9200/_bulk --data-binary @data/places.json
+curl -s -XPOST $url/_bulk --data-binary @data/places.json
 
-tail -n +2 data/postcodes.csv |
-  awk -F"," '{\
-name = $1
-key = toupper(name)
-sub(" ", "", key)
-without_spaces = name
-gsub(" ", "", without_spaces)
-lat  = $5
-long = $6
-print "{ \"index\" :  { \"_index\" : \"geo\", \"_type\" : \"postcode\", \"_id\" : \"" key "\" } }\
-{ \"name\" : [\"" name "\", \"" without_spaces "\"], \"latitude\" : "lat", \"longitude\" : " long " }" }' > data/postcodes.json
-
-split -l 200000 data/postcodes.json zz
-
-for f in `ls zz*`; do
-  curl -s -XPOST localhost:9200/_bulk --data-binary @$f
-done
-
-rm zz*
-
-tail -n +2 data/postcode_outcodes.csv |
-  awk -F"," '{\
-name = $2
-key = name
-lat  = $3
-long = $4
-print "{ \"index\" :  { \"_index\" : \"geo\", \"_type\" : \"outcode\", \"_id\" : \"" key "\" } }\
-{ \"name\" : [\"" name "\"], \"latitude\" : "lat", \"longitude\" : " long " }" }' > data/outcodes.json
-
-curl -s -XPOST localhost:9200/_bulk --data-binary @data/outcodes.json
+# tail -n +2 data/postcodes.csv |
+#   awk -F"," '{\
+# name = $1
+# key = toupper(name)
+# sub(" ", "", key)
+# without_spaces = name
+# gsub(" ", "", without_spaces)
+# lat  = $5
+# long = $6
+# print "{ \"index\" :  { \"_index\" : \"geo\", \"_type\" : \"postcode\", \"_id\" : \"" key "\" } }\
+# { \"name\" : [\"" name "\", \"" without_spaces "\"], \"latitude\" : "lat", \"longitude\" : " long " }" }' > data/postcodes.json
+# 
+# split -l 200000 data/postcodes.json zz
+# 
+# for f in `ls zz*`; do
+#   curl -s -XPOST localhost:9200/_bulk --data-binary @$f
+# done
+# 
+# rm zz*
+# 
+# tail -n +2 data/postcode_outcodes.csv |
+#   awk -F"," '{\
+# name = $2
+# key = name
+# lat  = $3
+# long = $4
+# print "{ \"index\" :  { \"_index\" : \"geo\", \"_type\" : \"outcode\", \"_id\" : \"" key "\" } }\
+# { \"name\" : [\"" name "\"], \"latitude\" : "lat", \"longitude\" : " long " }" }' > data/outcodes.json
+# 
+# curl -s -XPOST localhost:9200/_bulk --data-binary @data/outcodes.json
